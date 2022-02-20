68f5bbeb4e4812c768f7e63d717d44eda173d977
Covarrubias ❤ <82588527+Coarr@users.noreply.github.com>
2022-02-19 22:39:43 -0300

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js");
 const  client = new Discord.Client();
 
 client.on("ready", () => {
-   console.log("Estoy listo!");
+   console.log("UmbrellaBot ready!");
 });
 client.on("message", (message) => {
-  if (message.content.startsWith("hola")) {
-    message.channel.send("Hola que tal!");
+  if (message.content.startsWith("/commands")) {
+    message.channel.send("Here put your commands");
   }else
-    if (message.content.startsWith("adios")) {
-    message.channel.send("Hasta luego!");
+    if (message.content.startsWith("/help")) {
+    message.channel.send("Here put your help message");
 });
-client.login("OTA4MDg2MzgyNTIwNTk4NTI5.YYwnWQ.PIxhHVAaPt_tWQmdtIVOZl-u_fw");
+client.login("PUT YOUR DISCORD TOKEN");
